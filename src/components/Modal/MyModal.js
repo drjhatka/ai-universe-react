f38f45d92ee5115608b127e34ef0a61c1d2b6fae
drjhatka <bishawjit12@gmail.com>
2023-03-26 00:19:59 +0600

    api broken not all features available as before

--- src/components/Modal/MyModal.js
+++ src/components/Modal/MyModal.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
 const MyModal = ({closeModal, data}) => {
-    
+    console.log(data.description)
     return (
-        <div>
+
              <div className='custom_modal' onClick={e=>{e.stopPropagation(); closeModal()}}>
                 
-                <div className="modal_content d-flex flex-column justify-content-between">
-                <div className="modal_header d-flex closeBtn justify-content-end">
-                        <div className=''><span style={{color:"red"}} onClick={closeModal} >&times;</span></div>
+                <div className="modal_content d-flex flex-column">
+
+                    <div className="modal_header d-flex closeBtn justify-content-end">
+                        <div className=''>
+                            <span style={{color:"red"}} onClick={closeModal} >&times;</span>
+                        </div>
                     </div>
+
                     <div className="modal_body">
-                        <p>{data.name}</p>
+                        <div className='row'>
+                            <div className="col-md-6">
+                                <h4>{data.description? data.description:"Description not available"}</h4>   
+                            </div>
+                            <div className='col-md-6'>
+                                <div className='row'>
+                                    <div className="col-md-12">
+                                        <img className='modal_image' src={data.image} alt="" />
+                                    </div>
+                                    <div className="col-md-12">
+                                        <img className='modal_image' src={data.image} alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                   
                 </div>
             </div>
-        </div>
+
     );
 };
 
